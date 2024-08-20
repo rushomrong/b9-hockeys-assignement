8865e458d12192f63e6060fe767f9cb2dca65079
rushomrong <rushomarak@gmail.com>
2024-08-21 01:32:28 +0600

    Slider banner section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {},
     colors: {
       "btn-primary-r": ["#FF4240"],
+      " active_color": ["#FF4240"],
+      search_btn: ["#89898B"],
     },
   },
   plugins: [require("daisyui")],
