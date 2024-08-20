07cc7488a5729beb168cb5bec5549256cc5e48b4
rushomrong <rushomarak@gmail.com>
2024-08-21 03:51:50 +0600

    banner slide section complete done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
     extend: {},
     colors: {
       "btn-primary-r": ["#FF4240"],
-      " active_color": ["#FF4240"],
+      active_color: ["#FF4240"],
       search_btn: ["#89898B"],
+      card_color: ["#131318"],
+      primary_black: ["#000000"],
     },
   },
   plugins: [require("daisyui")],
